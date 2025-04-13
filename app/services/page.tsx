9ca8b62ff674dacc1a8ed7b52614afdb4962a7de
FlyingGrass <emre.bozqurt@gmail.com>
2025-04-13 16:46:25 +0300

    Update page.tsx

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -6,26 +6,26 @@ export default function Team() {
       <div className="mx-auto pb-20 max-sm:pb-12">
         <h1 className="text-6xl max-sm:text-3xl mt-16 max-sm:mt-8 text-center font-bold">Our Services</h1>
 
-        <ul className="mt-12 max-w-2xl mx-auto space-y-4 text-xl max-sm:text-lg">
-          <li className="p-4 bg-[#121212] rounded-lg max-sm:max-w-[80%] border border-accent">
+        <ul className="mt-12 max-w-2xl flex justify-center flex-col max-sm:items-center max-sm:justify-start mx-auto space-y-4 text-xl max-sm:text-lg">
+          <li className="p-4 bg-[#121212] rounded-lg max-sm:w-[85%] border border-accent">
             🎤 Wedding Organizations
           </li>
-          <li className="p-4 bg-[#121212] rounded-lg max-sm:max-w-[80%] border border-accent">
+          <li className="p-4 bg-[#121212] rounded-lg max-sm:w-[85%] border border-accent">
             🎶 Live Music Performances
           </li>
-          <li className="p-4 bg-[#121212] rounded-lg max-sm:max-w-[80%] border border-accent">
+          <li className="p-4 bg-[#121212] rounded-lg max-sm:w-[85%] border border-accent">
             🎭 Corporate Event Planning
           </li>
-          <li className="p-4 bg-[#121212] rounded-lg max-sm:max-w-[80%] border border-accent">
+          <li className="p-4 bg-[#121212] rounded-lg max-sm:w-[85%] border border-accent">
             🎧 Sound System Rentals
           </li>
-          <li className="p-4 bg-[#121212] rounded-lg max-sm:max-w-[80%] border border-accent">
+          <li className="p-4 bg-[#121212] rounded-lg max-sm:w-[85%] border border-accent">
             💡 Lighting & Stage Design
           </li>
-          <li className="p-4 bg-[#121212] rounded-lg max-sm:max-w-[80%] border border-accent">
+          <li className="p-4 bg-[#121212] rounded-lg max-sm:w-[85%] border border-accent">
             🎨 Event Branding & Marketing
           </li>
-          <li className="p-4 bg-[#121212] rounded-lg max-sm:max-w-[80%] border border-accent">
+          <li className="p-4 bg-[#121212] rounded-lg max-sm:w-[85%] border border-accent">
             🍾 VIP Hospitality Services
           </li>
         </ul>
